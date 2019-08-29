5308583465d16a255e855185a44e978e1925b9b8
Abdulhalim <aabdullodzhanov2@huskers.unl.edu>
2019-08-29 11:44:42 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Abdulkhalim Abdullodzhanov	
+ * Date: 2019/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Abdulkhalim Abdullodzhanov!\n");
+
+  return 0;
+}
