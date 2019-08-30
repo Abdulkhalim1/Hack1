00b4c7db48536cc38fb5d77e57db1ec41c4cf553
Abdulhalim <aabdullodzhanov2@huskers.unl.edu>
2019-08-29 22:16:30 -0500

    Finished labwork
    finished undone work that couldn't understand

--- hello.c
+++ hello.c
@@ -1,15 +1,17 @@
 /**
- * Author: Abdulkhalim Abdullodzhanov	
+ * Author: Abdulkhalim Abdullodzhanov
  * Date: 2019/08/26
- *
+ * Collaborator Eli Lundstrom
  * A simple hello world program in C
  *
  */
 #include<stdlib.h>
+#include<stdio.h>//kjhghfk
 
 int main(int argc, char **argv) {
 
-  printf("Abdulkhalim Abdullodzhanov!\n");
+  printf("Abdulkhalim Abdullodzhanov\n");
+  printf("Undecided\n");
 
   return 0;
 }
